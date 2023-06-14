13baea43ba1af17bec79116e553bee894d1beaa9
daksh0110 <dakshmahajan0110@gmail.com>
2023-06-14 14:53:26 +0530

    Added MongoDB server link

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
  
-mongoose.connect();
+mongoose.connect(process.env.MONGO_URI);
  
  
 const itemsSchema = {
