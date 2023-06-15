2895439e8431b00c906030da80f9cdd17c13dd14
daksh0110 <dakshmahajan0110@gmail.com>
2023-06-15 09:25:38 +0530

    d

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 app.set('view engine', 'ejs');
  
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static("Public"));
+
  
 const connectDB = async () => {
   try {
